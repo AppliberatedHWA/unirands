900f295d57505b1e11ecd593a14df347fc9a155d
Appliberated <develop@appliberated.com>
2017-11-06 20:03:49 +0200

    Implement parameters; organize and cleanup code and styles

--- js/main.js
+++ js/main.js
@@ -1,102 +1,88 @@
 "use strict";
 
+import Utils from "/js/utils.js";
+
+
 const MAX_CODE_POINT = 0x10FFFF;
+const MAX_PLANE = 16;
+// const MAX_CODE_POINT = 128;
 
-var makeMovable = function (target) {
+const DEFAULT_DELAY = 500;
+const MIN_DELAY = 0;
+const MAX_DELAY = 60 * 1000;
 
-    target.onmousedown = function (event) {
-        var initialXOffset = target.offsetLeft - event.pageX;
-        var initialYOffset = target.offsetTop - event.pageY;
 
-        document.onmousemove = function (event) {
-            
-            target.style.left = event.pageX + initialXOffset + "px";
-            target.style.top = event.pageY + initialYOffset + "px";
-            // console.log(target);
-        };
-
-        document.onmouseup = function () {
-            document.onmousemove = null;
-            document.onmouseup = null;
-        };
-
-        return false;
-    };
-};
 
 class App {
 
-    static getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-    }
-
-    // static onMouseDown(e) {
-    //     document.body.dataset.moveTarget = e.target;
-    //     document.addEventListener("mousemove", App.onMouseMove, true);
-    //     e.target.dataset.offsetX = e.target.offsetLeft - event.pageX;
-    //     e.target.dataset.offsetY = e.target.offsetTop - event.pageY;
-    // }
-
-    // static onMouseMove(e) {
-    //     let target = document.body.dataset.moveTarget;
-    //     console.log(target);
-    //     target.style.left = e.pageX + target.dataset.offsetX;
-    //     target.style.top = e.pageY + target.dataset.offsetY;
-    // }
-
-    // static onMouseUp(e) {
-    //     document.removeEventListener("mousemove", App.onMouseMove, true);
-    // }
-
-    static onClick(event) {
-        // let url = `http://www.fileformat.info/info/unicode/char/${event.target.dataset.codePoint}/index.htm`;
-        // window.open(url, "_blank");
-    }
-
-    static onDblClick(e) {
-        // document.body.removeChild(e.target);
+    static onDblClick(event) {
         let url = `http://www.fileformat.info/info/unicode/char/${event.target.dataset.codePoint}/index.htm`;
         window.open(url, "_blank");
     }
 
     static onContextMenu(event) {
         event.preventDefault();
-        // document.body.removeChild(event.target);
-        if (event.target == document.activeElement) {
-            let url = `http://www.fileformat.info/info/unicode/char/${event.target.dataset.codePoint}/index.htm`;
-            window.open(url, "_blank");
-        }
+        event.target.blur();
     }
 
 
     addNewChar() {
-        let codePoint = App.getRandomInt(0, MAX_CODE_POINT);
+        let codePoint = Utils.getRandomIntInclusive(0, this.maxCodePoint);
         let char = String.fromCodePoint(codePoint);
 
         let newCharElement = document.createElement("span");
         newCharElement.innerText = char;
         newCharElement.dataset.codePoint = codePoint.toString(16).toUpperCase();
-        newCharElement.style.left = App.getRandomInt(0, window.innerWidth) + "px";
-        newCharElement.style.top = App.getRandomInt(0, window.innerHeight) + "px";
+        newCharElement.style.left = Utils.getRandomIntInclusive(0, window.innerWidth) + "px";
+        newCharElement.style.top = Utils.getRandomIntInclusive(0, window.innerHeight) + "px";
         newCharElement.tabIndex = 0;
-        // newCharElement.addEventListener("mousedown", App.onMouseDown);
-        // newCharElement.addEventListener("mouseup", App.onMouseUp);
-        // console.log(newCharElement);
-
-        newCharElement.addEventListener("click", App.onClick);
         newCharElement.addEventListener("dblclick", App.onDblClick);
-        // newCharElement.addEventListener("contextmenu", App.onContextMenu);
-        // makeMovable(newCharElement);
+        newCharElement.addEventListener("contextmenu", App.onContextMenu);
+
         document.body.appendChild(newCharElement);
     }
 
-    constructor() {
-        setInterval(() => this.addNewChar(), 500);
+    static update() {
+        document.title = document.body.childElementCount + " unirands";
     }
 
 
+    parseParams() {
+        let plane = MAX_PLANE;
+
+        let query = window.location.search.substring(1);
+        if (!query.length) {
+            // We have no parameters, so use the default settings
+            this.maxCodePoint = MAX_CODE_POINT;
+            this.delay = DEFAULT_DELAY;
+        } else {
+            // We have parameters, so try to parse them and update current settings
+            let params = query.split("-").map(Number);
+            plane = Number.isInteger(params[0]) ? Utils.clamp(params[0], 0, 16) : 16;
+            this.maxCodePoint = (plane + 1) * (0xFFFF + 1) - 1;
+            this.delay = Number.isInteger(params[1]) ? Utils.clamp(params[1], MIN_DELAY, MAX_DELAY) : DEFAULT_DELAY;
+        }
+
+        let maxHex = this.maxCodePoint.toString(16).toUpperCase();
+        console.log(`Generating random Unicode characters between 0 and U+${maxHex} (plane ${plane}), with a delay ` +
+                    `of ${this.delay} milliseconds.`);
+    }
+
+    constructor() {
+
+        // alert("test");
+        // Parse any query params and update settings
+        this.parseParams();
+
+        // Start generating random Unicode characters
+        setInterval(() => this.addNewChar(), this.delay);
+
+        // Update the number of random Unicode characters in the document title
+        setInterval(() => App.update(), 1000);
+
+        // Disable the document context menu
+        document.addEventListener("contextmenu", (event) => event.preventDefault());
+    }
 }
 
 window.addEventListener("load", () => new App());
